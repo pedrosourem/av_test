3eb534aece1620272354bf8b100bc477c26818f6
pedrosouto18@gmail.com <pedrosouto18@gmail.com>
2023-03-05 17:43:46 +0000

    Editing parse_date macro (adding curly brackets to the date_string parameters)

--- macros/parse_date.sql
+++ macros/parse_date.sql
@@ -1,12 +1,12 @@
 {% macro parse_date(date_string) %}
     (SELECT 
         CASE 
-            WHEN CAST(RIGHT(date_string, 2) AS INTEGER) BETWEEN 0 AND CAST(RIGHT(CAST(DATE_TRUNC('YEAR', CURRENT_DATE()) AS VARCHAR), 2) AS INTEGER) THEN 
-                 TO_DATE(LEFT(date_string, CHARINDEX('/', date_string)-1) || '/' || 
-                 LEFT(RIGHT(date_string, LEN(date_string) - CHARINDEX('/', date_string)), CHARINDEX('/', RIGHT(date_string, LEN(date_string) - CHARINDEX('/', date_string)))-1) || '/' ||
-                 '20'|| RIGHT(date_string, 2), 'MM/DD/YYYY') 
-            ELSE TO_DATE(LEFT(date_string, CHARINDEX('/', date_string)-1) || '/' || 
-                 LEFT(RIGHT(date_string, LEN(date_string) - CHARINDEX('/', date_string)), CHARINDEX('/', RIGHT(date_string, LEN(date_string) - CHARINDEX('/', date_string)))-1) || '/' ||
-                 '19'|| RIGHT(date_string, 2), 'MM/DD/YYYY')   
+            WHEN CAST(RIGHT({{ date_string }}, 2) AS INTEGER) BETWEEN 0 AND CAST(RIGHT(CAST(DATE_TRUNC('YEAR', CURRENT_DATE()) AS VARCHAR), 2) AS INTEGER) THEN 
+                 TO_DATE(LEFT({{ date_string }}, CHARINDEX('/', {{ date_string }})-1) || '/' || 
+                 LEFT(RIGHT({{ date_string }}, LEN({{ date_string }}) - CHARINDEX('/', {{ date_string }})), CHARINDEX('/', RIGHT({{ date_string }}, LEN({{ date_string }}) - CHARINDEX('/', {{ date_string }})))-1) || '/' ||
+                 '20'|| RIGHT({{ date_string }}, 2), 'MM/DD/YYYY') 
+            ELSE TO_DATE(LEFT({{ date_string }}, CHARINDEX('/', {{ date_string }})-1) || '/' || 
+                 LEFT(RIGHT({{ date_string }}, LEN({{ date_string }}) - CHARINDEX('/', {{ date_string }})), CHARINDEX('/', RIGHT({{ date_string }}, LEN({{ date_string }}) - CHARINDEX('/', {{ date_string }})))-1) || '/' ||
+                 '19'|| RIGHT({{ date_string }}, 2), 'MM/DD/YYYY')   
         END)
 {% endmacro %}
